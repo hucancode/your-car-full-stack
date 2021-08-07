a73572a9bafeb7f7e0cbce975140206a55ca321a
hucancode <hucancode@gmail.com>
2021-08-07 09:44:16 +0900

    use carousel for top cars

--- src/app/containers/homepage/topCars.tsx
+++ src/app/containers/homepage/topCars.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { Car } from "../../components/car";
 import { ICar } from "../../model/car";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
+import './topCars.css'
+
+SwiperCore.use([Pagination]);
 
 const Container = styled.div`
     ${tw`
@@ -38,47 +45,66 @@ const CarsContainer = styled.div`
         justify-center
         mt-7
         md:mt-10
+        select-none
     `}
 `;
 
 export function TopCars() {
-    const testCar: ICar = {
-        name: "Audi S3",
-        mileage: "10k",
-        thumbnailSrc:
-          "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
-        dailyPrice: 70,
-        monthlyPrice: 1600,
-        gearType: "Auto",
-        gas: "Petrol",
-      };
-    
-      const testCar2: ICar = {
-        name: "Honda City 5",
-        mileage: "20k",
-        thumbnailSrc:
-          "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
-        dailyPrice: 50,
-        monthlyPrice: 1500,
-        gearType: "Auto",
-        gas: "Petrol",
-      };
-    const testCar3: ICar = {
-        name: "Honda City 5 Alpha",
-        mileage: "10k",
-        thumbnailSrc:
-          "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
-        dailyPrice: 50,
-        monthlyPrice: 1200,
-        gearType: "Auto",
-        gas: "Petrol",
-      };
-    return <Container>
-        <Title>Explore our top deals</Title>
-        <CarsContainer>
-            <Car {...testCar} />
-            <Car {...testCar2} />
-            <Car {...testCar3} />
-        </CarsContainer>
-    </Container>
+  const testCar: ICar = {
+    name: "Audi S3",
+    mileage: "10k",
+    thumbnailSrc:
+      "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+    dailyPrice: 70,
+    monthlyPrice: 1600,
+    gearType: "Auto",
+    gas: "Petrol",
+  };
+
+  const testCar2: ICar = {
+    name: "Honda City 5",
+    mileage: "20k",
+    thumbnailSrc:
+      "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+    dailyPrice: 50,
+    monthlyPrice: 1500,
+    gearType: "Auto",
+    gas: "Petrol",
+  };
+  const testCar3: ICar = {
+    name: "Honda City 5 Alpha",
+    mileage: "10k",
+    thumbnailSrc:
+      "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+    dailyPrice: 50,
+    monthlyPrice: 1200,
+    gearType: "Auto",
+    gas: "Petrol",
+  };
+  const breakPoints = {
+    "@0.75": {
+      "slidesPerView": 1
+    },
+    "@1.00": {
+      "slidesPerView": 2
+    },
+    "@1.50": {
+      "slidesPerView": 3
+    }
+  };
+  return <Container>
+    <Title>Explore our top deals</Title>
+    <CarsContainer>
+      <Swiper
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        breakpoints={breakPoints}
+      >
+        <SwiperSlide><Car {...testCar} /></SwiperSlide>
+        <SwiperSlide><Car {...testCar2} /></SwiperSlide>
+        <SwiperSlide><Car {...testCar3} /></SwiperSlide>
+        <SwiperSlide><Car {...testCar} /></SwiperSlide>
+      </Swiper>
+    </CarsContainer>
+  </Container>
 }
