f5410e1101d22d4700f931087f78155da47a80e0
hucancode <hucancode@gmail.com>
2021-08-07 20:15:42 +0900

    car read/create

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,14 +1,26 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CarsService } from "./cars.service";
+import { NewCarInput } from "./dto/new-car.input";
+import { Car } from "./entities/cars";
 
 @Resolver()
 export class CarsResolver {
     constructor(private service: CarsService) {
-
     }
 
-    @Query((returns) => String)
-    public async cars() {
-        return "Welcome to Your Car shop";
+    @Query((returns) => [Car])
+    public async cars(): Promise<Car[]> {
+        return await this.service.getAllCars().catch((err) => {
+            throw err;
+        });
+    }
+
+    @Mutation((returns) => Car)
+    public async addCar(@Args("data") data: NewCarInput): Promise<Car>
+    {
+        const car = await this.service.addCar(data).catch((err) => {
+            throw err;
+        });
+        return car;
     }
 }
