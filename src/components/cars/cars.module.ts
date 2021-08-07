54a97fd4fce61b18da513a37474de0b318c7a631
hucancode <hucancode@gmail.com>
2021-08-07 19:40:42 +0900

    setup graphql and cars module

---
+++ src/components/cars/cars.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { CarsResolver } from "./cars.resolver";
+import { CarsService } from "./cars.service";
+
+@Module({
+    imports: [],
+    providers: [CarsService, CarsResolver],
+    exports: [CarsService],
+})
+export class CarsModule {
+
+}
