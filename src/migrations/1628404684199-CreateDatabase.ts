253bc6a5c918ab855c51ef2e58eec7275a1b0c80
hucancode <hucancode@gmail.com>
2021-08-08 15:43:05 +0900

    setup production build

---
+++ src/migrations/1628404684199-CreateDatabase.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateDatabase1628404684199 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.createDatabase("yourcar", true);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropDatabase("yourcar", true);
+    }
+
+}
